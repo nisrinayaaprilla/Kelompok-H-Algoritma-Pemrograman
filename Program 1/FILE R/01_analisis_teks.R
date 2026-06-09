2beada3bda33f87615c08426696e9b94d89514b9
Jihan Nur Khanifah <jihan.khanifah@mhs.unsoed.ac.id>
2026-06-10 05:19:41 +0700

    Add files via upload

---
+++ Program 1/FILE R/01_analisis_teks.R
@@ -0,0 +1,16 @@
+#Program 1 - Menghitung jumlah kata dan jumlah kalimat dari sebuah teks
+
+teks <- readline(prompt = "Masukkan teks: ")
+
+#Menghilangkan spasi di awal/akhir
+teks <- trimws(teks)
+
+#Menghitung jumlah kalimat berdasarkan titik
+jumlah_kalimat <- nchar(gsub("[^.]", "", teks))
+
+#Menghitung jumlah kata
+jumlah_kata <- length(strsplit(teks, "\\s+")[[1]])
+
+cat(sprintf("Teks tersebut memuat %d kalimat dan %d kata.\n", jumlah_kalimat, jumlah_kata))
+
+main()
